478a55f1a72cecb98e6bf8ddd63e1c42f05dc41e
Shrey Vaity <43662680+Svaity@users.noreply.github.com>
2020-11-04 12:01:47 -0500

    Firebase authentication

--- fb-frontend/src/firebase.js
+++ fb-frontend/src/firebase.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    // this is where your firebase config goes
+    apiKey: "AIzaSyCwDft8rPjt9kRHTsTadiMPPZgajF_wtmM",
+  authDomain: "facebook-clone-aa2e3.firebaseapp.com",
+  databaseURL: "https://facebook-clone-aa2e3.firebaseio.com",
+  projectId: "facebook-clone-aa2e3",
+  storageBucket: "facebook-clone-aa2e3.appspot.com",
+  messagingSenderId: "693923136528",
+  appId: "1:693923136528:web:1439ecaa129102e16c7695",
+  measurementId: "G-JM46XHFMQ3"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
