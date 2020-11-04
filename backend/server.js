27ec7ef17f5c7b1a0a952319e28c3d3bc541955a
Shrey Vaity <43662680+Svaity@users.noreply.github.com>
2020-11-04 12:46:48 -0500

    Connection with MongoDB
    Setting up 2 connection for images and text

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,27 @@ app.use(bodyParser.json())
 app.use(cors())
 
 //db config
+const mongoURI = "mongodb+srv://admin:z0CgO5srFEZjqA6f@cluster0.ocfvv.mongodb.net/fbdb?retryWrites=true&w=majority"
 
+const conn = mongoose.createConnection(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
+mongoose.connect(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', ()=>{
+    console.log("DB connected")
+})
+
+conn.once('open', ()=>{
+    console.log("DB connected")
+})
 //api_routes
 app.get("/", (req, res)=> res.status(200).send("hello worlds"))
 
